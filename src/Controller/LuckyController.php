8f7a56ca78976178f7c2c247cab86c067909131e
Pat <p171982@gmail.com>
2021-07-09 20:40:48 +0100

    changes

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,50 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/home", name="Home")
+     *
+     * 
+     */
+    public function number(): Response
+    {
+        $title = "Team DR";
+        $number = random_int(0, 100);
+        $menu = ['Home','Blog','Contact'];
+
+
+        $contents =  $this->renderView('lucky/home.html.twig', [
+            'number' => $number,
+            'title' => $title,
+            'menu' => $menu,
+        ]);
+        return new Response($contents);
+    }
+    /**
+     * @Route("lucky/contact",name="contact",methods={"GET"})
+     */
+    public function contact()
+    {
+        $phone = "123-456-789";
+        $email = "contact@teamdr.com";
+        $title = "Contact Us";
+        $menu = ['Home','Blog','Contact'];
+
+        $contact = $this->renderView('lucky/contact.html.twig', [
+            'phone' => $phone,
+            'email' => $email,
+            'title' => $title,
+            'menu' => $menu,
+        ]);
+        return new Response($contact);
+
+    }
+}
+
